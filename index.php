43e5251b6337107de41b094eeb58e752cffab4d0
LuksameeLunsiay <73680084+LuksameeLunsiay@users.noreply.github.com>
2020-10-30 12:47:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+  <head>
+    <title>This is me</title>
+    <meta charset="UTF-8">
+  <body>
+    <h1>ลักษมี ลั่นซ้าย ออม</h1>
+    <p>63070152</p>
+    <img src="Rabbit.jpeg">
+    <img src="rabbit2.jpg">
+    <br>
+    <a href="https://www.youtube.com/user/HEARTROCKERChannel">This is a Link</a>
+    <h2>-.-</h2>
+        <u1>
+            <li>เรื่องตลก(?)</li>
+        </u1>
+    <p>เพื่อนหนูชงกาแฟเป็นบาริสต้าอยู่</p>
+    <p>วันนึงมีสาวที่ชอบเดินผ่านม</p>
+    <p>เพื่อนเลยพูดว่า</p>
+    <p>.</p>
+    <p>..</p>
+    <p>...</p>
+    <p>อะชิชิป่ะเห้ย์ //เพลงแทนบ๋อยนะคะ เผื่อจะตลกค่ะ55555</p>
+  </body>
+</html>
